7fdd893c6403a5e76a998be4d7f641ad956088cb
Alexandre Fernandes dos Anjos <alexandre.feranjos@gmail.com>
2017-09-19 16:49:32 -0300

    Add files via upload

--- blink.py
+++ blink.py
@@ -1,12 +1,12 @@
-import mraa
-import time
+import mraa	# Controles de I/O
+import time	# Funcoes de  tempo
 
-led = mraa.Gpio(23)
-led.dir(mraa.DIR_OUT)
+led = mraa.Gpio(23)	# Declarando a variavel 'led' como um pino da placa
+led.dir(mraa.DIR_OUT)	# Declarando que o 'led' sera uma saida
 
-while True:
+while True:	# Executa indefinidamente o codigo
 
-	led.write(1)
-	time.sleep(0.2)
-	led.write(0)
-	time.sleep(0.2)
+	led.write(1)	# Liga o led
+	time.sleep(0.2)	# Aguarda 200ms 
+	led.write(0)	# Desliga o led
+	time.sleep(0.2)	# Aguarda 200ms
